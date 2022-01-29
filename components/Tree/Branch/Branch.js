4335e8d0896a84bd08e4e23c4f71dc6ecf7559eb
Alexander Komeiji <alexanderkomeiji@gmail.com>
2022-01-30 01:53:48 +0200

    build don't crash

--- components/Tree/Branch/Branch.js
+++ components/Tree/Branch/Branch.js
@@ -43,7 +43,7 @@ const Branch = ({ item, level, itemLast }) => {
   const hasChildren = item.children ? true : false
   let itemLeaf = () => {
     //!item.children?.some((elem, i)=>elem.children) &&
-    return item.children?.filter((elem) => !elem.children).at(-1)
+    return item?.children?.filter((elem) => !elem.children).at(-1)
   }
 
   if (hasChildren) {
